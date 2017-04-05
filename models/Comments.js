431485e74435e990298fe3f149fa3fe41be0f619
alexis-fisher <alexisfisher1@gmail.com>
2017-04-05 22:42:10 +0000

    All fields working

--- models/Comments.js
+++ models/Comments.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 var CommentSchema = new mongoose.Schema({
   title: String,
+  session: String,
+  url: String,
+  speaker: String,
   upvotes: {type: Number, default: 0},
 });
 CommentSchema.methods.upvote = function(cb) {
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,19 +1,47 @@
 var comment = window.angular.module('comment', [])
 
-app.controller('MainCtrl', MainCtrl)
+comment.factory('pictureFetcher',pictureFetcher)
+comment.controller('MainCtrl', MainCtrl)
 
+function pictureFetcher($http) {
+  var API_ROOT = 'comments'
+  return {
+    get: function () {
+      return $http
+        .get(API_ROOT)
+        .then(function (resp) {
+          return resp.data
+        })
+    },
+    post: function (formData) {
+      return $http
+        .post(API_ROOT,formData)
+        .then(function (resp) {
+          console.log("Post worked");
+        })
+    }
+  }
+}
 
-function MainCtrl ($scope,$http) {
-  
-    $scope.comments = [];
-    $scope.addComment = function() {
-      var newcomment = {title:$scope.formContent,upvotes:0};
-      $scope.formContent='';
-      $http.post('/comments', newcomment).success(function(data){
-        $scope.comments.push(data);
-      });
-    };
-    $scope.upvote = function(comment) {
+function MainCtrl ($scope,$http, pictureFetcher) {
+  $scope.comments = [];
+
+  pictureFetcher.get()
+    .then(function (data) {
+      $scope.comments = data
+    })
+
+  $scope.addComment = function() {
+    var newcomment = {title:$scope.Title,session:$scope.Session,url:$scope.Url,speaker:$scope.Speaker,upvotes:0};
+    pictureFetcher.post(newcomment);
+    $scope.comments.push(newcomment);
+    $scope.Session='';
+    $scope.Url='';
+    $scope.Speaker='';
+    $scope.Title='';
+  };
+    
+$scope.upvote = function(comment) {
       return $http.put('/comments/' + comment._id + '/upvote')
         .success(function(data){
           console.log("upvote worked");
